76efe1c053d05c8804e06d49fb0d4db79b4d94c7
Sudipsamanta21 <sudipsamanta0010@gmail.com>
2026-07-17 10:00:00 +0530

    Updated EmailService and Ap

--- Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
+++ Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
@@ -1,34 +1,3 @@
-//package com.Sudip.Money_manager.service;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class EmailService {
-//
-//    private final JavaMailSender mailSender;
-//
-//    @Value("${spring.mail.properties.mail.from}")
-//    private String fromEmail;
-//    public void sendEmail(String to, String subject, String body) {
-//        try {
-//            SimpleMailMessage message = new SimpleMailMessage();
-//            message.setFrom(fromEmail);
-//            message.setTo(to);
-//            message.setSubject(subject);
-//            message.setText(body);
-//            mailSender.send(message);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Failed to send email: " + e.getMessage(), e);
-//        }
-//    }
-//
-//
-//}
 
 package com.Sudip.Money_Manager.service;
 
@@ -45,7 +14,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.properties.mail.from}")
+    @Value("${spring.mail.properties.mail.smtp.from}")
     private String fromEmail;
 
     public void sendEmail(String to, String subject, String body) {
@@ -53,17 +22,12 @@ public class EmailService {
         try {
 
             MimeMessage message = mailSender.createMimeMessage();
-
-            MimeMessageHelper helper =
-                    new MimeMessageHelper(message, true, "UTF-8");
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
             helper.setFrom(fromEmail);
             helper.setTo(to);
             helper.setSubject(subject);
-
-            // true = HTML Content
             helper.setText(body, true);
-
             mailSender.send(message);
 
         } catch (Exception e) {
