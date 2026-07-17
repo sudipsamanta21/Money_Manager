1765a22561735ca0aee0c64824824fe68bace3f4
Sudipsamanta21 <sudipsamanta0010@gmail.com>
2026-07-17 10:33:34 +0530

    Upadted EmailService

--- Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
+++ Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
@@ -1,3 +1,41 @@
+//
+//package com.Sudip.Money_Manager.service;
+//
+//import jakarta.mail.internet.MimeMessage;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class EmailService {
+//
+//    private final JavaMailSender mailSender;
+//
+//    @Value("${spring.mail.properties.mail.smtp.from}")
+//    private String fromEmail;
+//
+//    public void sendEmail(String to, String subject, String body) {
+//
+//        try {
+//
+//            MimeMessage message = mailSender.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+//
+//            helper.setFrom(fromEmail);
+//            helper.setTo(to);
+//            helper.setSubject(subject);
+//            helper.setText(body, true);
+//            mailSender.send(message);
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to send email: " + e.getMessage(), e);
+//        }
+//    }
+//}
+
 package com.Sudip.Money_Manager.service;
 
 import lombok.RequiredArgsConstructor;
