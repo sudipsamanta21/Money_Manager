7844fd8dc5b1adfe75214c4f1ec3702f39d1e52d
Sudip Samanta <168456650+sudipsamanta21@users.noreply.github.com>
2026-07-17 10:13:31 +0530

    Update EmailService to send emails via Brevo API
    Refactor email sending to use Brevo API instead of JavaMailSender.

--- Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
+++ Money_Manager/src/main/java/com/Sudip/Money_Manager/service/EmailService.java
@@ -1,37 +1,58 @@
-
 package com.Sudip.Money_Manager.service;
 
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    @Value("${brevo.api.key}")
+    private String apiKey;
 
-    @Value("${spring.mail.properties.mail.smtp.from}")
-    private String fromEmail;
+    @Value("${brevo.sender.email}")
+    private String senderEmail;
+
+    @Value("${brevo.sender.name}")
+    private String senderName;
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public void sendEmail(String to, String subject, String body) {
 
-        try {
+        String url = "https://api.brevo.com/v3/smtp/email";
 
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("accept", "application/json");
+        headers.set("api-key", apiKey);
 
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(body, true);
-            mailSender.send(message);
+        Map<String, Object> request = Map.of(
+                "sender", Map.of(
+                        "name", senderName,
+                        "email", senderEmail
+                ),
+                "to", List.of(
+                        Map.of("email", to)
+                ),
+                "subject", subject,
+                "htmlContent", body
+        );
 
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to send email: " + e.getMessage(), e);
+        HttpEntity<Map<String, Object>> entity =
+                new HttpEntity<>(request, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(url, entity, String.class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new RuntimeException("Failed to send email: " + response.getBody());
         }
     }
 }
